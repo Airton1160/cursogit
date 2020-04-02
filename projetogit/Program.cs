625fa3b078614b5db9d4ee28e395632917ffc8e7
Airton Diniz <liriungo@outlook.com.br>
2020-04-02 13:27:40 -0300

    alteracao opa manoooo

--- projetogit/Program.cs
+++ projetogit/Program.cs
@@ -7,6 +7,8 @@ namespace projetogit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("opaaa manooo");
+
         }
     }
 }
